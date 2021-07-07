e8d1d3b957bae3e0d1f69d775c68b56e08362a8b
John Newby <john@newby.org>
2021-07-07 08:11:54 +0200

    Fixes

--- compile-contract.sh
+++ compile-contract.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
+cd smart-contracts
 
 ligo compile-contract fa2_hooks/ligo/src/fa2_multi_asset.mligo multi_asset_main > fa2.tz
--- compile-storage.sh
+++ compile-storage.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-ligo compile-storage fa2_hooks/ligo/src/fa2_multi_asset.mligo multi_asset_main "`cat ../fa2-demo/storage.mligo`"
+cd smart-contracts
+
+ligo compile-storage fa2_hooks/ligo/src/fa2_multi_asset.mligo multi_asset_main "`cat ../storage.mligo`" > storage.tz
